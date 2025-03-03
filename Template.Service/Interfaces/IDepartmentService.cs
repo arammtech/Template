980ac505c5c223078e12bf1f34cd559be4e1f1ce
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-03-04 02:03:00 +0300

    Movign Departemnt to a single file

--- Template.Service/Interfaces/IDepartmentService.cs
+++ Template.Service/Interfaces/IDepartmentService.cs
@@ -20,19 +20,4 @@ namespace Template.Service.Interfaces
         Task DeleteAsync(int id);
     }
 
-    public interface IEmployeeService : IBaseService
-    {
-        EmployeeDto? Get(Expression<Func<Employee, bool>> filter);
-        Task<EmployeeDto?> GetAsync(Expression<Func<Employee, bool>> filter);
-        IEnumerable<EmployeeDto> GetAll(Expression<Func<Employee, bool>>? filter = null);
-        Task<IEnumerable<EmployeeDto>> GetAllAsync(Expression<Func<Employee, bool>>? filter = null);
-        void Add(EmployeeDto employeeDto);
-        Task AddAsync(EmployeeDto employeeDto);
-        void AddRange(IEnumerable<EmployeeDto> employeeDtos);
-        Task AddRangeAsync(IEnumerable<EmployeeDto> employeeDtos);
-        void Update(int id, EmployeeDto employeeDto);
-        Task UpdateAsync(int id, EmployeeDto employeeDto);
-        void Delete(int id);
-        Task DeleteAsync(int id);
-    }
 }
