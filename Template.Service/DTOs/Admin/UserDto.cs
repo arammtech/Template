fdffcdd14200c934079effec55b5035526696867
Mery Kassis <MeryKassis48@gmail.com>
2025-03-04 23:50:25 +0300

    Update userDto

--- Template.Service/DTOs/Admin/UserDto.cs
+++ Template.Service/DTOs/Admin/UserDto.cs
@@ -7,7 +7,7 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string? Phone { get; set; }
-        public string Role { get; set; }
+        public List<string> Role { get; set; }
         public bool IsLocked { get; set; }
     }
 }
