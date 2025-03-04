b66f1b209c26712089f1a29d748ebdc69fa921a7
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-03-04 14:25:58 +0300

    Adding Pagging method for getting users

---
+++ Template.Service/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using static Template.Domain.Global.Result;
+using Template.Service.DTOs.Admin;
+
+namespace Template.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<PaginatedResult<UserDto>> GetUsersAsync(int page, int pageSize);
+    }
+}
