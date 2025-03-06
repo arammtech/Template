b4d20114302d14ba1d58e5bc02f9b1bb50eafdd7
Mery Kassis <MeryKassis48@gmail.com>
2025-03-06 20:54:48 +0300

    User profile

--- Template.Web/Areas/Admin/Controllers/SettingsController.cs
+++ Template.Web/Areas/Admin/Controllers/SettingsController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Template.Domain.Identity;
+using Template.Repository.EntityFrameworkCore.Context;
+using Template.Service.DTOs.Admin;
+using Template.Service.Interfaces;
 using Template.Utilities.Identity;
 
 namespace Template.Web.Areas.Admin.Controllers
@@ -8,9 +14,38 @@ namespace Template.Web.Areas.Admin.Controllers
     [Authorize(Roles = AppUserRoles.RoleAdmin)]
     public class SettingsController : Controller
     {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserService _userService;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+
+        public SettingsController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager,  IUserService userService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _roleManager = roleManager;
+
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+
+            try
+            {
+                UserDto admin = await _userService.GetUserByIdAsync(1);
+
+                return View(admin);
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+
         }
     }
 }
--- Template.Web/Areas/Admin/Controllers/UserController.cs
+++ Template.Web/Areas/Admin/Controllers/UserController.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Template.Domain.Identity;
 using Template.Repository.EntityFrameworkCore.Context;
+using Template.Service.DTOs.Admin;
+using Template.Service.Implementations;
+using Template.Service.Interfaces;
 using Template.Utilities.Identity;
+using Template.Web.Areas.Admin.ViewModels;
 
 namespace Template.Web.Areas.Admin.Controllers
 {
@@ -13,17 +17,75 @@ namespace Template.Web.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly AppDbContext _context;
+        private readonly IUserService _userService;
+        private readonly RoleManager<ApplicationRole> _roleManager;
 
-        public UserController(UserManager<ApplicationUser> userManager,AppDbContext context)
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, IUserService userService)
         {
             _userManager = userManager;
-            _context = context;
+            _userService = userService;
+            _roleManager = roleManager;
+
         }
+
 
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            try
+            {
+                UserDto user = await _userService.GetUserByIdAsync(id);
+
+                return View(user);
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> editRole(int id)
+        {
+            try
+            {
+                UserDto user = await _userService.GetUserByIdAsync(id);
+
+                ChangeUserRoleDto changeUserRoleDto = new();
+                changeUserRoleDto.userId = user.Id;
+                changeUserRoleDto.oldRole = string.Join(", ", user.Role);
+                changeUserRoleDto.newRole = string.Join(", ", user.Role);
+                changeUserRoleDto.Roles = _roleManager.Roles.ToList();
+
+                return View(changeUserRoleDto);
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+          
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> editRole(int id, ChangeUserRoleDto changeUserRoleDto)
+        {
+            try
+            {
+                UserDto user = await _userService.GetUserByIdAsync(id);
+
+                
+
+                return RedirectToAction("index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
         }
     }
 }
