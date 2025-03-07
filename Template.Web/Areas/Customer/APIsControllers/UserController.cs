53d2e674158f49b8828fe95559d8576277e192c9
Mery Kassis <MeryKassis48@gmail.com>
2025-03-07 23:02:23 +0300

    Customer Profile and delete his account

---
+++ Template.Web/Areas/Customer/APIsControllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using System.Security.Claims;
+using Template.Domain.Identity;
+using Template.Repository.EntityFrameworkCore.Context;
+using Template.Service.DTOs.Admin;
+using Template.Service.Interfaces;
+
+namespace Template.Web.Areas.Customer.APIsControllers
+{
+    [Route("api/customer/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IUserService _userService;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, SignInManager<ApplicationUser> signInManager, IUserService userService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+      
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            ClaimsIdentity claimsIdentity = (ClaimsIdentity)User?.Identity;
+            int userId = int.Parse(claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            try
+            {
+                var result = await _userService.DeleteUserAsync(userId);
+                if (result.IsSuccess)
+                {
+                    await _signInManager.SignOutAsync();
+                    return Ok(new { success = true, message = "Account deleted successfully!" });
+                }
+                else
+                {
+                    return StatusCode(500, new { success = false, message = "An error occurred while deleting the user." });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { success = false, message = "An error occurred while deleting the user." });
+            }
+        }
+
+
+
+    }
+}
---
+++ Template.Web/Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Template.Domain.Identity;
+using Template.Service.DTOs.Admin;
+using Template.Service.Interfaces;
+
+namespace Template.Web.Areas.Customer.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserService _userService;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, IUserService userService, IWebHostEnvironment webHostEnvironment)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _roleManager = roleManager;
+            _webHostEnvironment = webHostEnvironment;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Profile()
+        {
+            try
+            {
+                ClaimsIdentity claimsIdentity = (ClaimsIdentity)User?.Identity;
+                int userId = int.Parse(claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+                var user = await _userService.GetUserByIdAsync(userId);
+
+                return View(user);
+            }
+            catch
+            {
+                return View("Error");
+
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> editProfile(int id)
+        {
+            try
+            {
+                var user = await _userService.GetUserByIdAsync(id);
+
+                return View(user);
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> editProfile(int id, UserDto user)
+        {
+            try
+            {
+               if(ModelState.IsValid)
+                {
+                    var result = await _userService.UpdateUserAsync(user);
+
+                    if (result.IsSuccess)
+                    {
+                        return View(user);
+
+                    }
+                }
+
+
+                return View(user);
+            }
+            catch
+            {
+                return View("Error");
+
+            }
+        }
+        public async Task<IActionResult> Delete()
+        {
+            try
+            {
+                ClaimsIdentity claimsIdentity = (ClaimsIdentity)User?.Identity;
+                int userId = int.Parse(claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+                var user = await _userService.GetUserByIdAsync(userId);
+
+                return View(user);
+            }
+            catch
+            {
+                return View("Error");
+
+            }
+        }
+
+
+    }
+}
